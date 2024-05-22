075ff67d218e6096484fe18a39a942bbe9a62748
reumeum <boreum1111@gmail.com>
2024-05-22 17:00:35 +0900

    InsertBookFormAction

---
+++ doread/src/main/java/kr/book/action/InsertBookFormAction.java
@@ -0,0 +1,18 @@
+package kr.book.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import kr.controller.Action;
+
+public class InsertBookFormAction implements Action{
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		HttpSession session = request.getSession();
+		
+		return null;
+	}
+
+}
--- doread/src/main/java/kr/book/dao/BookDAO.java
+++ doread/src/main/java/kr/book/dao/BookDAO.java
@@ -3,6 +3,7 @@ package kr.book.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 
+import kr.book.vo.BookVO;
 import kr.util.DBUtil;
 
 public class BookDAO {
@@ -14,7 +15,7 @@ public class BookDAO {
 	private BookDAO() {};
 	
 	//책 정보 삽입
-	public void InsertBook(String book_name, String author, String publisher, String publish_date, int price, int stock, String book_img, String book_category) throws Exception{
+	public void InsertBook(BookVO book) throws Exception{
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		String sql = null;
@@ -22,16 +23,6 @@ public class BookDAO {
 		conn = DBUtil.getConnection();
 		sql = "INSERT book(book_num,book_name,author,publisher,publish_date,price,stock,book_img,book_category) VALUES (book_seq,?,?,?,?,?,?,?,?)";
 		
-		pstmt = conn.prepareStatement(sql);
-		pstmt.setString(1, book_name);
-		pstmt.setString(2, author);
-		pstmt.setString(3, publisher);
-		pstmt.setString(4, publish_date);
-		pstmt.setInt(5, price);
-		pstmt.setInt(6, stock);
-		pstmt.setString(7, book_img);
-		pstmt.setString(8, book_category);
 		
-		pstmt.executeUpdate();
 	};
 }
