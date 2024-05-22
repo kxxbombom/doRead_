04171439bd716aae5d6be716a18dab1370cb680b
bomee <bm9742@gmail.com>
2024-05-22 17:15:16 +0900

    memberVO,DAO

--- doread/src/main/java/kr/member/dao/MemberDAO.java
+++ doread/src/main/java/kr/member/dao/MemberDAO.java
@@ -18,15 +18,29 @@ public class MemberDAO {
 	public void insertMember(MemberVO member)throws Exception{
 		Connection conn = null;
 		PreparedStatement pstmt = null;
+		PreparedStatement pstmt2 = null;
+		PreparedStatement pstmt3 = null;
 		String sql = null;
 		try {
 			conn = DBUtil.getConnection();
+			
+			conn.setAutoCommit(false);
+			
+			//회원번호 생성
+			//member insert
+			//member_detail insert
+			
+			
+			
 			
 			sql = "INSERT INTO member_detail (mem_num, name, passwd, phone, email, zipcode, address1, address2, photo, reg_date, modify_date) VALUES (member_seq.nextval,?,?,?,?,?,?,?,?,SYSDATE,?)";
 			
 			pstmt = conn.prepareStatement(sql);
 			
 			pstmt.setString(1, sql);
+			pstmt.setString(2, member.getPasswd());
+			pstmt.setString(3, member.getPhone());
+			
 		}catch(Exception e) {
 			throw new Exception(e);
 		}finally {
