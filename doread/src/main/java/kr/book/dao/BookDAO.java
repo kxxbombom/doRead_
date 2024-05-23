72c37c2f0b7d9fe9e5f7d4a0e6c5543f50570f50
kyj-ingg <kyj.ingg@gmail.com>
2024-05-23 15:23:20 +0900

    책 등록 모델클래스

--- doread/src/main/java/kr/book/dao/BookDAO.java
+++ doread/src/main/java/kr/book/dao/BookDAO.java
@@ -19,10 +19,25 @@ public class BookDAO {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		String sql = null;
-		
-		conn = DBUtil.getConnection();
-		sql = "INSERT book(book_num,book_name,author,publisher,publish_date,price,stock,book_img,book_category) VALUES (book_seq,?,?,?,?,?,?,?,?)";
-		
-		    
+		try {
+			conn = DBUtil.getConnection();
+			sql = "INSERT INTO book (book_num,book_name,author,publisher,publish_date,price,stock,book_category,book_img) VALUES (book_seq.nextval,?,?,?,?,?,?,?,?)";
+			
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, book.getBook_name());
+			pstmt.setString(2, book.getAuthor());
+			pstmt.setString(3, book.getPublisher());
+			pstmt.setString(4, book.getPublish_date());
+			pstmt.setInt(5, book.getPrice());
+			pstmt.setInt(6, book.getStock());
+			pstmt.setInt(7, book.getBook_category());
+			pstmt.setString(8,book.getBook_img());
+			
+			pstmt.executeUpdate();
+		}catch(Exception e) {
+			throw new Exception(e);
+		}finally {
+			DBUtil.executeClose(null, pstmt, conn);
+		}
 	};
 }
