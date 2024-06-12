8c96aef803df1fd69611088fd25c47a35077d534
YOONSIWON <ytldnjs@naver.com>
2024-06-12 11:16:37 +0900

    story - 관리자도 삭제할 수 있게 수정

--- doread/src/main/java/kr/storyboard/action/StoryDetailAction.java
+++ doread/src/main/java/kr/storyboard/action/StoryDetailAction.java
@@ -2,6 +2,7 @@ package kr.storyboard.action;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import kr.controller.Action;
 import kr.storyboard.dao.StoryBoardDAO;
@@ -12,6 +13,8 @@ public class StoryDetailAction implements Action{
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		HttpSession session = request.getSession();
+		Integer user_auth = (Integer)session.getAttribute("user_auth");
 		//글 번호 반환
 		int s_num = Integer.parseInt(request.getParameter("s_num"));
 						
@@ -25,6 +28,7 @@ public class StoryDetailAction implements Action{
 		sb.setS_content(StringUtil.useBrNoHTML(sb.getS_content()));
 				
 		request.setAttribute("sb", sb);
+		request.setAttribute("user_auth",user_auth);
 				
 				
 		return "/WEB-INF/views/story/storyDetail.jsp";
--- doread/src/main/webapp/WEB-INF/views/story/storyDetail.jsp
+++ doread/src/main/webapp/WEB-INF/views/story/storyDetail.jsp
@@ -31,7 +31,7 @@
 			<ul class="board-btn-container">
 				<li class="board-btn">
 					<span class="board-cbtn" style="position:absolute; z-index:999; right:0; bottom:10px;"><a href="" onclick="return false;"><img src="${pageContext.request.contextPath}/upload/ellipsis-vertical-outline.svg" width="25px"></a></span>
-					<c:if test="${user_num == sb.mem_num}">
+					<c:if test="${user_num == sb.mem_num || user_auth==9}">
 					<ul class="btn-hide">
 		            	<li><a href="storyUpdateForm.do?s_num=${sb.s_num}">수정</a></li>
 		            	<li><a href="#" id ="delete_btn">삭제</a></li>
